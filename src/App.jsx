f11320febce2db27f509756614ece72211d45022
nivarSh <nivarsherwani0@gmail.com>
2024-11-16 18:04:33 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import { Header } from "./components/Header";
+import { Tabs } from "./components/Tabs";
+import { TodoList } from "./components/TodoList";
+import { TodoInput } from "./components/TodoInput";
+
+import { useState, useEffect } from "react";
+
+function App() {
+  // const todos = [
+  //   { input: 'Hello! Add your first todo!', complete: true },
+  //   { input: 'Get the groceries!', complete: false },
+  //   { input: 'Learn how to web design', complete: false },
+  //   { input: 'Say hi to gran gran', complete: true },
+  // ]
+
+  const [selectedTab, setSelectedTab] = useState("Open");
+
+  const [todos, setTodos] = useState([
+    { input: "Hello! Add your first todo!", complete: true },
+  ]);
+
+  function handleAddTodo(newTodo) {
+    const newTodoList = [...todos, { input: newTodo, complete: false }];
+    setTodos(newTodoList);
+    handleSaveData(newTodoList)
+  }
+
+  function handleCompleteTodo(index) {
+    // update/edit/modify
+    let newTodoList = [...todos];
+    let completedTodo = todos[index];
+    completedTodo["complete"] = true;
+    newTodoList[index] = completedTodo;
+    setTodos(newTodoList);
+    handleSaveData(newTodoList)
+  }
+
+  function handleDeleteTodo(index) {
+    let newTodoList = todos.filter((val, valIndex) => {
+      return valIndex !== index;
+    });
+    setTodos(newTodoList);
+    handleSaveData(newTodoList)
+  }
+
+  function handleSaveData(currTodos) {
+    localStorage.setItem("todo-app", JSON.stringify({ todos: currTodos }));
+  }
+
+  useEffect(() => {
+    if (!localStorage || !localStorage.getItem("todo-app")) {
+      return;
+    } // if local storage isnt ready to access do nothing until ready
+    console.log("here");
+    let db = JSON.parse(localStorage.getItem("todo-app"));
+    setTodos(db.todos);
+  }, []);
+
+  return (
+    <>
+      <Header todos={todos} />
+      <Tabs
+        selectedTab={selectedTab}
+        setSelectedTab={setSelectedTab}
+        todos={todos}
+      />
+      <TodoList
+        todos={todos}
+        selectedTab={selectedTab}
+        handleDeleteTodo={handleDeleteTodo}
+        handleCompleteTodo={handleCompleteTodo}
+      />
+      <TodoInput handleAddTodo={handleAddTodo} />
+    </>
+  );
+}
+
+export default App;
