ffe59ea1edbc896aef720514651b0000b7f69524
nivarSh <nivarsherwani0@gmail.com>
2025-04-30 18:37:02 -0400

    organized codebase

--- src/components/WkdayCard.jsx
+++
@@ -1,8 +0,0 @@
-export function WkdayCard({ day, time, isCurrentDay }) {
-  return (
-    <div className={`card wkday-card ${isCurrentDay ? "highlight" : ""}`}>
-      <p>{day}</p>
-      <p>{time}</p>
-    </div>
-  );
-}
